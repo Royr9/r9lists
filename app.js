0f1ccc042725776ade8ba33570380501fd63b31f
roy <royramati99@gmail.com>
2023-01-06 11:13:20 +0200

    delete import from and type modul

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ const day = date.getDate();
 // mongoose/mongodb database: 
 mongoose.set('strictQuery', false);
 // to connecto locally:  mongodb://0.0.0.0:27017
-import mongoose from 'mongoose';
 mongoose.connect(process.env.DATABASE_URL || "mongodb+srv://roy-admin1:5ZOyBqWdHniRBpT4@todolistcluster1.r6ene6j.mongodb.net/todoListDB" , {useNewUrlParser: true});
 
 
