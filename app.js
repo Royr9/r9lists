54c6f0301a38296da431df29e407ff9e38893e5b
roy <royramati99@gmail.com>
2023-01-06 12:18:11 +0200

    changed mongoose.connect

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ const day = date.getDate();
  
 
 // mongoose/mongodb database: 
-mongoose.set('strictQuery', false);
-// to connecto locally:  mongodb://0.0.0.0:27017/todoListDB
-mongoose.connect(process.env.DATABASE_URL || "mongodb://0.0.0.0:27017/todoListDB" , {useNewUrlParser: true});
+// mongoose.set('strictQuery', false);
+// // to connecto locally:  mongodb://0.0.0.0:27017/todoListDB
+// mongoose.connect(process.env.DATABASE_URL || "mongodb+srv://roy-admin1:5ZOyBqWdHniRBpT4@todolistcluster1.r6ene6j.mongodb.net/todoListDB" , {useNewUrlParser: true});
 
   //atlas cluster:  mongodb+srv://roy-admin1:5ZOyBqWdHniRBpT4@todolistcluster1.r6ene6j.mongodb.net/todoListDB
 
